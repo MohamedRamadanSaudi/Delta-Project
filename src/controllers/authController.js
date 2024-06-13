6424d2da812e514991ef11efbe1ea166d13f2835
Mohamed Ramadan <MohamedRamadanSaudi@gmail.com>
2024-06-13 09:26:40 +0300

    Modified to make user or admin login by his phone not Email

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -91,17 +91,17 @@ exports.setPasswordAfterOTP = catchAsync(async (req, res, next) => {
 });
 
 exports.login = catchAsync(async function (req, res, next) {
-  const { email, password } = req.body;
+  const { phone, password } = req.body;
 
-  // 1) Check if email and password exist
-  if (!email || !password) {
-    return next(new AppError('Please provide email and password!', 400));
+  // 1) Check if phone and password exist
+  if (!phone || !password) {
+    return next(new AppError('Please provide phone and password!', 400));
   }
   // 2) Check if user exists && password is correct
-  const user = await User.findOne({ email: email }).select('+password');
+  const user = await User.findOne({ phone: phone  }).select('+password');
 
   if (!user || !(await user.correctPassword(password, user.password))) {
-    return next(new AppError('Incorrect email or password', 401));
+    return next(new AppError('Incorrect phone or password', 401));
   }
 
   // 3) If everything ok, send token to client
@@ -109,18 +109,18 @@ exports.login = catchAsync(async function (req, res, next) {
 });
 
 exports.adminLogin = catchAsync(async function (req, res, next) {
-  const { email, password } = req.body;
+  const { phone, password } = req.body;
 
-  // 1) Check if email and password exist
-  if (!email || !password) {
-    return next(new AppError('Please provide email and password!', 400));
+  // 1) Check if phone and password exist
+  if (!phone || !password) {
+    return next(new AppError('Please provide phone and password!', 400));
   }
 
   // 2) Check if user exists && password is correct
-  const user = await User.findOne({ email: email }).select('+password');
+  const user = await User.findOne({ phone: phone }).select('+password');
 
   if (!user || !(await user.correctPassword(password, user.password))) {
-    return next(new AppError('Incorrect email or password', 401));
+    return next(new AppError('Incorrect phone or password', 401));
   }
 
   // 3) Check if user has admin role
