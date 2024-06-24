7f801a29768aa166e6e6bf9c29fef0870bf2d3e6
Mohamed Ramadan <MohamedRamadanSaudi@gmail.com>
2024-06-24 08:25:28 +0300

    Fix bug

--- src/routes/orderRoutes.js
+++ src/routes/orderRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const orderController = require('../controllers/orderController');
-const auth = require('../middlewares/authMiddleware');
+const auth = require('../middlewares/AuthMiddleware');
 
 const router = express.Router();
 
