a125b740d8840ac9de6b82e60f9f78b6d00d482a
Mohamed Ramadan <MohamedRamadanSaudi@gmail.com>
2024-06-24 09:02:46 +0300

    Add when the user makes a maintenance request, checking if the user has any completed orders or not

--- src/controllers/maintenanceRequestController.js
+++ src/controllers/maintenanceRequestController.js
@@ -1,55 +1,69 @@
 const mongoose = require('mongoose');
 const MaintenanceRequest = require('../models/maintenanceRequestModel');
-const User = require('../models/userModel');
+const Order = require('../models/orderModel');
 const catchAsync = require('../utils/catchAsync');
 const AppError = require('../utils/appError');
 
 // Create a maintenance request
 exports.createMaintenanceRequest = catchAsync(async (req, res, next) => {
-    const { type, address, description, date, time } = req.body;
-    const { _id: userId } = req.user;
+  const { type, address, description, date, time } = req.body;
+  const { _id: userId } = req.user;
 
-    try {
-      // Check if the user already has an active request
-      const existingRequest = await MaintenanceRequest.findOne({
-        user: userId,
-        status: 'pending'
-      });
-  
-      if (existingRequest) {
-        return next(new AppError('You already have an active maintenance request', 400));
-      }
-  
-      // Create new maintenance request
-      const newRequest = await MaintenanceRequest.create({
-        type,
-        address,
-        description,
-        date,
-        time,
-        user: userId
-      });
-  
-      res.status(201).json({
-        status: 'success',
-        data: {
-          request: newRequest
-        }
-      });
-    } catch (error) {
-      return next(new AppError(error.message, 500));
+  // Check if the user already has an active request
+  const existingRequest = await MaintenanceRequest.findOne({
+    user: userId,
+    status: 'pending'
+  });
+
+  if (existingRequest) {
+    return next(new AppError('You already have an active maintenance request', 400));
+  }
+
+  // Create new maintenance request
+  const newRequest = await MaintenanceRequest.create({
+    type,
+    address,
+    description,
+    date,
+    time,
+    user: userId
+  });
+
+  res.status(201).json({
+    status: 'success',
+    data: {
+      request: newRequest
     }
   });
+});
+
+// Helper function to enhance requests with order info
+const enhanceRequestsWithOrderInfo = async (requests) => {
+  return await Promise.all(
+    requests.map(async (request) => {
+      const userOrders = await Order.find({ user: request.user._id });
+      const hasCompletedOrder = userOrders.some(order => order.status === 'Completed');
+      const hasAnyOrder = userOrders.length > 0;
+
+      return {
+        ...request.toObject(),
+        userHasCompletedOrder: hasCompletedOrder,
+        userHasOrder: hasAnyOrder
+      };
+    })
+  );
+};
 
 // Get all normal maintenance requests
 exports.getAllNormalRequests = catchAsync(async (req, res, next) => {
   const normalRequests = await MaintenanceRequest.find({ type: 'normal' }).populate('user').populate('address');
+  const enhancedRequests = await enhanceRequestsWithOrderInfo(normalRequests);
 
   res.status(200).json({
     status: 'success',
-    results: normalRequests.length,
+    results: enhancedRequests.length,
     data: {
-      requests: normalRequests
+      requests: enhancedRequests
     }
   });
 });
@@ -57,12 +71,13 @@ exports.getAllNormalRequests = catchAsync(async (req, res, next) => {
 // Get all urgent maintenance requests
 exports.getAllUrgentRequests = catchAsync(async (req, res, next) => {
   const urgentRequests = await MaintenanceRequest.find({ type: 'urgent' }).populate('user').populate('address');
+  const enhancedRequests = await enhanceRequestsWithOrderInfo(urgentRequests);
 
   res.status(200).json({
     status: 'success',
-    results: urgentRequests.length,
+    results: enhancedRequests.length,
     data: {
-      requests: urgentRequests
+      requests: enhancedRequests
     }
   });
 });
