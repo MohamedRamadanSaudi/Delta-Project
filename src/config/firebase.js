488b46f9cd61f9de8cb4011bce1a007005add191
Mohamed Ramadan <MohamedRamadanSaudi@gmail.com>
2024-06-24 08:22:38 +0300

    Encode FIREBASE_SERVICE_ACCOUNT

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -1,5 +1,5 @@
 const admin = require('firebase-admin');
-const serviceAccount = require('./firebaseServiceAccount.json');
+const serviceAccount = JSON.parse(Buffer.from(process.env.FIREBASE_SERVICE_ACCOUNT, 'base64').toString('utf8'));
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
