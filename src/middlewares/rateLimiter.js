486b5fd9e6b112bd27281c662f7b20163e74e8d0
Mohamed Ramadan <91095064+MohamedRamadanSaudi@users.noreply.github.com>
2024-08-29 05:39:25 +0300

    modify rate limit to be block for just 15 minutes not an hour

--- src/middlewares/rateLimiter.js
+++ src/middlewares/rateLimiter.js
@@ -3,8 +3,10 @@ const rateLimit = require('express-rate-limit');
 // Rate limiter middleware for non-admin users
 const limiter = rateLimit({
   max: 100,
-  windowMs: 60 * 60 * 1000, // 1 hour
-  message: 'Too many requests from this IP, please try again in an hour!'
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  message: 'Too many requests from this IP, please try again later.',
+  standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
+  legacyHeaders: false, // Disable the `X-RateLimit-*` headers
 });
 
 const rateLimiter = (req, res, next) => {
